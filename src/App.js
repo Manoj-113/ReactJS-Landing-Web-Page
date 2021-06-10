94f9f7bff298bc36ee31523379e071f2d8eff903
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-06-10 11:45:14 +0530

    Install material ui core and icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import {makeStyles} from '@material-ui/core/styles';
 import './App.css';
 
-function App() {
-  return (
+const useStyles = makeStyles((theme) => ({}));
+
+export default function () {
+
+  const classes = useStyles();
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Island</h1>
     </div>
   );
 }
 
-export default App;
