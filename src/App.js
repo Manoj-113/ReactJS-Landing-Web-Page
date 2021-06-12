3e671113a16e9c245359666b12d9a032c1ebcadd
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-06-12 10:28:27 +0530

    Changed and add background picture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import react from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import './App.css';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+    root:{
+        minHeight:"100vh",
+        backgroundImage: 'url(${process.env.PUBLIC_URL + "/assets/bg.jpg"})'
+    },
 
-export default function () {
-
-  const classes = useStyles();
-  return(
-    <div className="App">
-      <h1>Island</h1>
-    </div>
-  );
+}));
+export default function App() {
+    const classes = useStyles();
+    return (
+        <div>
+            className={classes.root}
+        </div>
+    )
 }
-
