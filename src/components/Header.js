8a94fcdf1d774a4d5a5737fe0441a08859c9d83f
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-06-12 11:02:15 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import {AppBar, IconButton} from '@material-ui/core';
-import SortIcon from '@material-ui/icons/sort';
 
 const useStyles = makeStyles((theme) =>({
 
@@ -12,9 +11,7 @@ export default function Header(){
         <div>
             <AppBar>
                 <h1>MyIsland</h1>
-                <IconButton>
-                    <SortIcon/>
-                </IconButton>
+                
             </AppBar>
 
         </div>
